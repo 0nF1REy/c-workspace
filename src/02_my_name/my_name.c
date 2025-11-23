e2d9470d601ac3adf6721a6f434c28b149b1e868
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-22 22:54:03 -0300

    main: feat (02_my_name) - Added new example showing basic name output in C
    - Added a new example module implementing a simple C program that prints a name, expanding the repository's collection of introductory examples.

---
+++ src/02_my_name/my_name.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Meu nome é Alan Ryan da Silva Domingues;\n");
+    printf("Tenho 21 anos;\n");
+    printf("Moro em Itapeva - SP;\n");
+    printf("Estou estudando C!\n");
+    return 0;
+}
