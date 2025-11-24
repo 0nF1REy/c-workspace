fd6f338ede492d1346fdfcb261634abb90d7ceb1
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-24 00:18:29 -0300

    main: feat (05_type_sizes) - Added example to demonstrate size of float, double, and long double in C

---
+++ src/05_type_sizes/type_sizes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Tamanho de float: %zu bytes\n", sizeof(float));
+    printf("Tamanho de double: %zu bytes\n", sizeof(double));
+    printf("Tamanho de long double: %zu bytes\n", sizeof(long double));
+
+    return 0;
+}
