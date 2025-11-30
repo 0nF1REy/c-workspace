3fa285812319f4a79f421916e4d3dc8229bd20ed
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-30 19:34:08 -0300

    main: feat (21_while_loops) - Added while loop example to count from 1 to 10

---
+++ src/21_while_loops/count_1_to_10/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int i = 1;
+
+    printf("--- Contando de 1 a 10 com WHILE ---\n");
+
+    while (i <= 10) {
+
+        printf("%d\n", i);
+        
+        i++; 
+    }
+    
+    return 0;
+}
