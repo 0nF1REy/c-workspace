1d2f989c5e3d6332d44765f0d36daeb4e15d0aaf
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-06 10:09:43 -0300

    main: refactor (24_arrays) - Cleaned up comments and improved code readability

--- src/24_arrays/02_grade_average/main.c
+++ src/24_arrays/02_grade_average/main.c
@@ -1,37 +1,29 @@
 #include <stdio.h>
 
-// Boa prática: Definir o tamanho como constante
 #define QTD_NOTAS 5
 
 int main(void) {
-    // DECLARAÇÃO: float notas[5];
-    // O computador reserva espaço para 5 números flutuantes seguidos.
+    
     float notas[QTD_NOTAS];
     float soma = 0.0;
-    float media = 0.0;
+    float media;
 
-    printf("--- SISTEMA DE NOTAS (ARRAYS) ---\n");
+    printf("--- SISTEMA DE NOTAS ---\n");
 
-    // 1. PREENCHENDO O ARRAY (Escrita)
+    // Entrada de dados
     for (int i = 0; i < QTD_NOTAS; i++) {
-        // Importante: Arrays começam no índice 0
-        // Por isso usamos (i + 1) apenas para mostrar ao usuário "Nota 1"
         printf("Digite a Nota %d: ", i + 1);
-        scanf("%float", &notas[i]);
-        
-        // Podemos ir somando enquanto lemos
+        scanf("%f", &notas[i]);
         soma += notas[i];
     }
 
-    // 2. LENDO O ARRAY (Leitura)
-    printf("\n--- Relatório ---\n");
-    printf("Notas registradas: ");
-    
+    // Exibição do relatório
+    printf("\nNotas registradas: ");
     for (int i = 0; i < QTD_NOTAS; i++) {
         printf("[%.1f] ", notas[i]);
     }
 
-    // 3. CÁLCULO FINAL
+    // Cálculo e Resultado
     media = soma / QTD_NOTAS;
     
     printf("\n\nMédia Final: %.2f\n", media);
