8f5e3a9a4d548dca769da8dce4b40bf9ac9981b1
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-12-11 18:27:57 -0300

    main: refactor (00_exercise_lists) - Improved sum-four-numbers exercise prompts and variable usage
    - Clarified user prompts and adjusted variable usage in the sum-four-numbers exercise to improve readability and input handling.

--- src/00_exercise_lists/01_c_language_course_2022_free/01_sum_four_numbers/main.c
+++ src/00_exercise_lists/01_c_language_course_2022_free/01_sum_four_numbers/main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
+    
     int n1, n2, n3, n4, soma;
 
-    printf("Digite quatro números inteiros:\n");
-    scanf("%d %d %d %d", &n1, &n2, &n3, &n4);
+    printf("Insira o primeiro número: ");
+    scanf("%d", &n1);
+
+    printf("Insira o segundo número: ");
+    scanf("%d", &n2);
+
+    printf("Insira o terceiro número: ");
+    scanf("%d", &n3);
+
+    printf("Insira o quarto número: ");
+    scanf("%d", &n4);
 
     soma = n1 + n2 + n3 + n4;
 
-    printf("Soma: %d\n", soma);
+    printf("Resultado da soma: %d\n", soma);
 
     return 0;
 }
